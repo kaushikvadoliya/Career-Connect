b8528f1e4215b6cdb03249d219c1ebdb40384fe4
kaushik <kaushikvadoliya009@gmail.com>
2025-10-10 17:30:11 +0530

    fix:make stepper component seprate

---
+++ src/components/Stepper/index.tsx
@@ -0,0 +1,113 @@
+import { useFormContext } from 'react-hook-form';
+import EducationAndExperience from '../EducationAndExperience';
+import PersonalInformation from '../PersonalInformation';
+import PortFolio from '../PortFolio';
+import ReviewAndSubmit from '../ReviewAndSubmit';
+import { UserDetails } from '../../types/userType';
+import { useState } from 'react';
+import { Alert, Image, Text, TouchableOpacity, View } from 'react-native';
+import Button from '../Button';
+import styles from './style';
+
+const steps = [
+  {
+    id: 'persoanl-info-form',
+    title: 'Personal Information',
+    content: <PersonalInformation />,
+  },
+  {
+    id: 'education-experience',
+    title: 'Educational & Experience',
+    content: <EducationAndExperience />,
+  },
+  {
+    id: 'porrtfolio',
+    title: 'Portfolio & Preferences',
+    content: <PortFolio />,
+  },
+  {
+    id: 'review-submit',
+    title: 'Review & Submit',
+    content: <ReviewAndSubmit />,
+  },
+];
+
+const Stepper = () => {
+  const { handleSubmit } = useFormContext<UserDetails>();
+  const [activeStep, setActiveStep] = useState<number>(0);
+
+  const onSubmit = (data: UserDetails) => {
+    setActiveStep(activeStep + 1);
+    console.log(data);
+  };
+
+  const backPage = () => {
+    setActiveStep(activeStep - 1);
+  };
+
+  const onSave = () => {
+    Alert.alert('Data is Submitted');
+  };
+
+  return (
+    <>
+      <View style={styles.mainContainer}>
+        <View style={styles.stepContainer}>
+          {steps.map((_, index) => {
+            return (
+              <TouchableOpacity
+                key={index}
+                style={
+                  activeStep === index
+                    ? styles.activeButton
+                    : styles.inActiveButton
+                }
+                onPress={() => setActiveStep(index)}
+              >
+                <Text style={styles.buttonText}>Step {index + 1}</Text>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+        <View style={styles.headerContainer}>
+          <Image
+            style={styles.image}
+            source={require('../../assests/logo.png')}
+          />
+          <Text style={styles.logoText}>CareerConnct</Text>
+        </View>
+        <Text style={styles.headerTitle}>{steps[activeStep].title}</Text>
+        {steps[activeStep].content}
+      </View>
+
+      <View style={styles.mainButtonContainer}>
+        {activeStep > 0 && (
+          <Button
+            onPress={backPage}
+            text="Back"
+            backgroudColor="grey"
+            style={styles.button}
+          />
+        )}
+        {activeStep < steps.length - 1 && (
+          <Button
+            onPress={handleSubmit(onSubmit)}
+            text="Next"
+            backgroudColor="#1e90ff"
+            style={styles.button}
+          />
+        )}
+        {activeStep === steps.length - 1 && (
+          <Button
+            onPress={handleSubmit(onSave)}
+            text="Save"
+            backgroudColor="#1e90ff"
+            style={styles.button}
+          />
+        )}
+      </View>
+    </>
+  );
+};
+
+export default Stepper;
--- src/screens/FormScreen/style.ts
+++ src/screens/FormScreen/style.ts
@@ -5,59 +5,6 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 16,
   },
-  headerContainer: {
-    flexDirection: 'row',
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  logoText: {
-    fontSize: 18,
-    fontWeight: '700',
-  },
-  image: {
-    height: 40,
-    width: 40,
-    gap: 10,
-  },
-  button: {
-    flex: 1,
-  },
-  stepContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  headerTitle: {
-    fontSize: 28,
-    fontWeight: '800',
-    marginTop: 30,
-  },
-  activeButton: {
-    backgroundColor: '#1e90ff',
-    paddingHorizontal: 20,
-    paddingVertical: 6,
-    borderRadius: 10,
-    justifyContent: 'center',
-  },
-  inActiveButton: {
-    backgroundColor: 'grey',
-    paddingHorizontal: 20,
-    paddingVertical: 6,
-    borderRadius: 10,
-    justifyContent: 'center',
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 12,
-    fontWeight: '500',
-  },
-  mainButtonContainer: {
-    flexDirection: 'row',
-    gap: 20,
-    justifyContent: 'space-between',
-  },
-  mainContainer: {
-    flex: 1,
-  },
 });
 
 export default styles;
